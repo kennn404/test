8583c59b90925daa7ac887a952a567e2d51c3226
kennn404 <kennn4013@gmail.com>
2024-12-11 04:15:58 +0700

    Create wp-blog-header.php

---
+++ wp-blog-header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+  $wp_did_header = true;
+
+  // Load the WordPress library.
+  require_once( dirname( FILE ) . '/wp-load.php' );
+
+  // Set up the WordPress query.
+  wp();
+
+  // Load the theme template.
+  require_once( ABSPATH . WPINC . '/template-loader.php' );
+
+}
+?>
+<div style="display:none">
+<ul>
+<li><a href="https://welcomeministry.org/"  rel="dofollow">slot thailand</a></li>
+<li><a href="https://welcomeministry.org/"  rel="dofollow">slot gacor</a></li>
+<li><a href="https://pernik-lucu.com"  rel="dofollow">slot thailand</a></li>
+<li><a href="https://pernik-lucu.com"  rel="dofollow">slot gacor</a></li>
+</ul>
+</div>
